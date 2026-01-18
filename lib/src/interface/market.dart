3afcc380d3d920b40d6dec0fd970abef2f53cd31
normidar <normidar7@gmail.com>
2026-01-18 20:24:46 +0900

    Add interval export to market_interface.dart and implement getKlineHistory method in Market interface

--- lib/src/interface/market.dart
+++ lib/src/interface/market.dart
@@ -1,9 +1,14 @@
 import 'dart:async';
 
+import 'package:finance_kline_core/finance_kline_core.dart';
 import 'package:market_interface/src/type/instrument_type.dart';
 
 abstract class Market {
   FutureOr<List<String>> getInstruments({required InstrumentType type});
 
-  
+  FutureOr<OhlcvSeries> getKlineHistory({
+    required String instrument,
+    required String interval,
+    required int limit,
+  });
 }
